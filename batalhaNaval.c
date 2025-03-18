25077de3feee73978bdec9de1164cae73f97cf79
victoriamoraisdev <victoriamoraisnobre14@gmail.com>
2025-03-18 13:58:29 +0000

    adicionei dois navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 
 
-
 int main() {
    //Criando o tabuleiro do Jogo (Matriz 10 x 10)
-   int tabuleiro[10][10] = 
-   {
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-   };
+   int tabuleiro[10][10];
 
-   char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};//Linha do Tabuleiro
+   char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};//colunas
    
    printf("***** BATALHA NAVAL *****\n\n");
    printf("- Tabuleiro -\n\n");
@@ -27,32 +14,40 @@ int main() {
    printf("   ");//Para alinhar a linha
    for (int i = 0; i < 10; i++)
    {
-    printf("%c ", linha[i]);
+    printf("%c ", linha[i]);//Imprimi os números das colunas
    }
    printf("\n");
 
-   //Posição dos navios
-   for(int i = 0; i < 100; i++)//Navio na horizontal
-   {
-        tabuleiro[3][5] = 3;
-        tabuleiro[3][6] = 3;
-        tabuleiro[3][7] = 3;
-   }
-   
-   for(int i = 0; i < 100; i++)//Navio na vertical
-   {
-        tabuleiro[5][3] = 3;
-        tabuleiro[6][3] = 3;
-        tabuleiro[7][3] = 3;
-   }
-   
-   //Imprimi o Tabuleiro
+   //Exibi o Tabuleiro
    for (int i = 0; i < 10; i++)
    {
     printf("%2d ", i + 1);
     for(int j = 0; j < 10; j++)
     {
-       printf("%d ", tabuleiro[i][j]);
+      tabuleiro[i][j] = 0;//Inicalização da matriz(tabuleiro)
+    
+      //Posição dos navios
+      
+      //Na horizontal
+      tabuleiro[3][5] = 3;
+      tabuleiro[3][6] = 3;
+      tabuleiro[3][7] = 3;
+       
+      //Na vertical
+      tabuleiro[5][3] = 3;
+      tabuleiro[6][3] = 3;
+      tabuleiro[7][3] = 3;
+
+      //Na diagonal
+      tabuleiro[1][1] = 3;//Navio 1
+      tabuleiro[2][2] = 3;
+      tabuleiro[3][3] = 3;
+
+      tabuleiro[5][9] = 3;//Navio 2
+      tabuleiro[6][8] = 3;
+      tabuleiro[7][7] = 3;
+
+      printf("%d ", tabuleiro[i][j]);
     }
     printf("\n");
    }
